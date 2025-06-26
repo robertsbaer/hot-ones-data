3406bad4e73d34d5567c61972474fa8f3f4e4bb4
robertsbaer <robertsbaer@yahoo.com>
2025-06-26 14:38:49 -0400

    Changed url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hot-ones-data/',
+  base: '/', // Change this to root
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
