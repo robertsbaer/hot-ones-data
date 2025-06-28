0d42afcae4ca3c7a7f2c28882d93dc2ce18036b9
robertsbaer <robertsbaer@yahoo.com>
2025-06-27 22:41:40 -0400

    sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginSitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: 'https://hotones-data.com',
+      dynamicRoutes: ['/', '/data']
+    })
+  ],
   base: '/', // Change this to root
   build: {
     outDir: 'dist',
